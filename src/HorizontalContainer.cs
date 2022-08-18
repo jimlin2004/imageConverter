5d35d2e52d202ea7ad3976308829206e9df99a29
jimlin2004 <jim.lin2004@gmail.com>
2022-08-18 11:12:26 +0800

    first commit

---
+++ src/HorizontalContainer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageConverter.src
+{
+    public class HorizontalContainer
+    {
+        public List<Control> elements;
+
+        public HorizontalContainer()
+        {
+            elements = new List<Control>();
+        }
+
+        public HorizontalContainer(params Control[] items)
+        {
+            elements = new List<Control>();
+            elements.AddRange(items);
+        }
+
+        public void add(params Control[] controls)
+        {
+            elements.AddRange(controls);
+            return;
+        }
+
+        private int getMidlineLeftX(int midlineX, Control item)
+        {
+            return midlineX - item.Width / 2;
+        }
+
+        public void autoArrange(int width)
+        {
+            int unitWidth = width / (elements.Count + 1);
+            int midlineX = unitWidth;
+            int x = 0;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                x = getMidlineLeftX(midlineX, elements[i]);
+                elements[i].Location = new Point(x, elements[i].Location.Y);
+                midlineX += unitWidth;
+            }
+        }
+    }
+}
